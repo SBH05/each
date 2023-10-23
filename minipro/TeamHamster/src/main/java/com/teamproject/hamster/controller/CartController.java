1d972cdf80bbde847c0e9294bc0c40bc6c0cfbf3
SBH05 <seobohye5@gmail.com>
2023-10-23 23:44:07 +0900

    second

--- minipro/TeamHamster/src/main/java/com/teamproject/hamster/controller/CartController.java
+++ minipro/TeamHamster/src/main/java/com/teamproject/hamster/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.teamproject.hamster.controller;
 
+import com.teamproject.hamster.dto.ProductDTO;
 import com.teamproject.hamster.view.ListView;
 
 import java.lang.reflect.Array;
@@ -11,9 +12,9 @@ public class CartController {
     ListView LM = new ListView();
     Scanner sc = new Scanner(System.in);
 
-    public void shopBasket(ArrayList select) {
+    SelectListController sel = new SelectListController();
+    public void shopBasket(ArrayList <ProductDTO> select) {
 
-       // System.out.println(select);
 
         while (true) {
 
@@ -24,34 +25,98 @@ public class CartController {
             System.out.print("번호를 입력 하세요 : ");
             int num = sc.nextInt();
             switch (num) {
-                case 1:   break; // 선택한 상품 사이즈 변경 Key 값을 기입
-                case 2: //cartCancel(select); break;
-                case 3:  break;
-                case 4://  PaymentController.print(select);
-                        break; //select
-            }
+                case 1:
+                    changeSize(select);
+                    break;
+                case 2:
+                    cartCancel(select);
+                    break;
+                case 3:
+                    sel.selectMenu ();
+                    break;
+                case 4:
+                   // PaymentController pc = new PaymentController();
+                    //pc.print(select);
 
+            }
         }
     }
 
 
-    public void cartCancel(ArrayList cancelList) {
-
-      //  List<ProductDTO> cancelList = new ArrayList<>();
-
+    // 선택 취소 메소드
+    public void cartCancel(ArrayList<ProductDTO> cancel) { // 선택 취소 메소드
+        for (int i = 0; i < cancel.size(); i++) { //담은 목록 출력
+            System.out.print(cancel.get(i).getName() + ", ");
+            System.out.print(cancel.get(i).getSize() + ", ");
+            System.out.println(cancel.get(i).getPrice() + " ");
+        }
+        System.out.println("어떤 것을 취소하겠습니까? ");
+        sc.nextLine();
+        System.out.print("신발 이름을 입력하세요 : ");
+        String st = sc.nextLine();
+        System.out.print("선택한 사이즈를 입력하세요 : ");
+        int siz = sc.nextInt();
+        for (int i = 0; i < cancel.size(); i++) {
+            if (st.equals(cancel.get(i).getName()) && siz == cancel.get(i).getSize()) {
+                System.out.println(cancel.get(i).getName() + " " + cancel.get(i).getSize() + "취소 되었습니다.");
+                cancel.remove(i);//제거 기능(인덱스)
+                System.out.println("장바구니에 돌아 갑니다.");
+                shopBasket(cancel); //장바구니에 돌아감
+            }
+        }
+        System.out.println("잘못 입력하셨습니다");
     }
 
 
+    //사이즈 변경 메소드
+    public void changeSize(ArrayList<ProductDTO> change) {
+
+        for (int i = 0; i < change.size(); i++) { //담은 목록 출력
+            System.out.print(change.get(i).getName() + ",  ");
+            System.out.print(change.get(i).getSize() + ",  ");
+            System.out.println(change.get(i).getPrice() + " ");
+        }
+
+        System.out.println("어떤 것을 변경합니까? ");
+        sc.nextLine();
+        System.out.print("신발 이름을 입력하세요 : ");
+        String st = sc.nextLine();
+        System.out.print("선택한 사이즈를 입력하세요 : ");
+        int siz = sc.nextInt();
+        int pr = 0;
+
+        ListView mv = new ListView();
+        ArrayList<ProductDTO> productList = mv.getProductList();
 
 
-    public void changeSize() { //스위치
+        for (int j = 0; j < change.size(); j++) {
+            if (st.equals(change.get(j).getName()) && siz == change.get(j).getSize()) {
+                change.remove(j); //제거
+                for (int i = 0; i < productList.size(); i++) {
+                    if (st.equals(productList.get(i).getName())) {
+                        System.out.println("제품 이름 : " + productList.get(i).getName());
+                        System.out.println("제품 사이즈 : " + productList.get(i).getSize());
+                        System.out.println("제품 가격 : " + productList.get(i).getPrice());
+                    }
+                }
+                System.out.println("===변경 할 수 있는 사이즈 목록===");
 
+                System.out.print("변경할 사이즈를 입력 해주세요 : ");
+                int chan = sc.nextInt();
 
+                for (int i = 0; i < productList.size(); i++) {//추가 기능
+                    if (st.equals(productList.get(i).getName())) {
+                        pr = productList.get(i).getPrice();
+                        change.add(new ProductDTO(st, chan, pr));
+                        break;
+                    }
+                }
+                System.out.println(st + " " + chan + "사이즈로 변경 되었습니다.");
+                System.out.println("장바구니에 돌아 갑니다.");
+                shopBasket(change); //장바구니에 돌아감
+            }
+        }
+        System.out.println("잘못 입력하셨습니다");
     }
-
-
-
-
-
 
 }
